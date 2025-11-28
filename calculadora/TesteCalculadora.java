a8f414361ddc992280ad3b568ea4f24c1f9fb6d4
Gabriel Alesiunas <gabriel.alesiunas@yahoo.com.br>
2025-11-28 20:44:04 -0300

    Calculadora JAVA

---
+++ calculadora/TesteCalculadora.java
@@ -0,0 +1,15 @@
+package calculadora;
+
+public class TesteCalculadora {
+    public static void main(String[] args) {
+        Calculadora calc = new Calculadora();
+
+        // Testes rápidos
+        calc.calc(2, 3, "+"); //Esperado 5
+        calc.calc(10, 4, "-"); //Esperado 6
+        calc.calc(3, 3, "*"); //Esperado 15
+        calc.calc(8, 2, "/"); //Esperado 4
+        calc.calc(8, 0, "/");  //Divisão por zero 
+        calc.calc(5, 5, "x");  //Operação inválida
+    }
+}
